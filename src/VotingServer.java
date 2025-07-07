b2c3d01e40cbadaaff081a07e61de1294e64087c
Askar <nurbekovaskar099@gmail.com>
2025-07-07 15:14:09 +0600

    Created class for voyea server

---
+++ src/VotingServer.java
@@ -0,0 +1,10 @@
+import server.BasicServer;
+
+import java.io.IOException;
+
+public class VotingServer extends BasicServer {
+
+    protected VotingServer(String host, int port) throws IOException {
+        super(host, port);
+    }
+}
